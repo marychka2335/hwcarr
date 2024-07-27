e56953e009cf3de322cdc0c1c8bdf756ae5f7fdf
marychka2335 <marychka2335@icloud.com>
2024-07-27 11:43:54 +0300

    pages commit

---
+++ src/pages/CatalogPage.jsx
@@ -0,0 +1,15 @@
+import styles from './SharedStyles.module.css';
+import { FilterBar } from '../components/FilterBar/FilterBar';
+import { CamperList } from "../components/CamperList/CamperList";
+
+export const CatalogPage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <aside>
+        <FilterBar/>
+      </aside>
+      <CamperList/>
+    </div>
+    
+  );
+};
